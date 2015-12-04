9205db7f828114e23ae2565616f5217987578a9e
arseniy <arseniy.aganov@gmail.com>
2015-12-04 03:58:06 +0300

    Добавил функцию pow для Value. Реализовал кодировшик

--- Coder.cpp
+++ Coder.cpp
@@ -1,8 +1,5 @@
-//
-// Created by arsen on 04.12.2015.
-//
-
 #include "Coder.h"
+#include "Value.h"
 
 Coder::Coder(int m, int t) {
     this->m = m;
@@ -11,6 +8,18 @@ Coder::Coder(int m, int t) {
 
 vector<int> Coder::encode(vector<int> data) {
     vector<int> message;
+
+    for (int i = 0; i < data.size(); i++) {
+
+        Value symbol;
+        Value x = Value::pow(Value(2), i);
+
+        for (int j = 0; j < data.size(); j++) {
+            symbol = symbol + Value(data[j]) * Value::pow(x, j);
+        }
+
+        message.push_back(symbol.get_value());
+    }
 
     return message;
 }
