0daf599fc1bb157b1f947d7f299b19be094b1945
arseniy <arseniy.aganov@gmail.com>
2015-12-04 17:50:07 +0300

    Добавил оператор / и функцию get_inverse

--- main.cpp
+++ main.cpp
@@ -11,14 +11,10 @@ int main() {
     Coder coder = Coder(3, 2);
     Decoder decoder = Decoder(2, 3);
 
-    vector<int> message = coder.encode({4, 6, 7, 0, 0, 0, 0});
+    vector<int> message = coder.encode({4, 6, 7});
 
     for (int i = 0; i < message.size(); i++)
         cout << message[i] << " ";
-
-
-    vector<Value> testCode = {5,2,5,3,3,2,4};
-    vector<Value> decoded = decoder.decode(testCode);
 
     return 0;
 }
