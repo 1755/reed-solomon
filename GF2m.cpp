74a4a0a15af4cb4677aa9fdafedb0772cad73832
arseniy <arseniy.aganov@gmail.com>
2015-12-19 14:48:30 +0300

    new _ pow 2

--- GF2m.cpp
+++ GF2m.cpp
@@ -1,9 +1,13 @@
-#include "GF2m.h"
+#include "Value.h"
 
 GF2m *GF2m::field = 0;
 
 void GF2m::set_m(int dim) {
     m = dim;
+
+    for(int i = 0; i < get_capacity(); i++)
+        inverse.push_back(Value::pow(Value(i), get_capacity() - 2));
+
 }
 
 
